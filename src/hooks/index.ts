b8453aa3a64af3e72a0eedef36a8140577839a71
odanzhou <ihosalt@gmail.com>
2022-08-24 22:06:24 +0800

    Create index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useNewRef } from './useNewRef'
+export { default as useRequest } from './useRequest'
+export { default as useIsUpdate } from './useIsUpdate'
+export { default as useParams } from './useParams'
