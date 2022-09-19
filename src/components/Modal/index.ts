385ac6589563a6f66f96dcfc02f3050eacda11df
odanzhou <ihosalt@gmail.com>
2022-09-19 22:41:11 +0800

    Update index.ts

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,45 +1,6 @@
-import React, { memo } from 'react'
-import { Modal, Form } from 'antd'
-import { ModalProps } from 'antd/lib/modal/Modal'
-import { FormList, useForm, useFormState } from '../KForm'
+export * from './modal'
+import KModal from './modal'
+export default KModal
+export { default as ConfirmButton } from './confirmButton'
+export { default as ItemConfirm } from './itemConfirm'
 
-/**
- * 默认Modal 的 BodyStyle
- */
-const DefBodyStyle = { maxHeight: 450, overflow: 'auto' }
-
-/**
- * 表单 Modal
- * @param {{
- *  modalConf?: Omit<ModalProps, 'onOk'>
- * } & Parameters<useForm>[0]} props 
- */
-const KModal = (props) => {
-  const { modalConf, ...fromConf } = props
-  const { formListProps, onOkClick, confirmLoading } = useForm(fromConf)
-  return (
-    <Modal maskClosable={false} bodyStyle={DefBodyStyle} confirmLoading={confirmLoading} {...modalConf}  onOk={onOkClick}>
-      <FormList {...formListProps} />
-    </Modal>
-  )
-}
-
-const KModalWithForm = memo(Form.create()(KModal))
-
-/**
- * 表单 Modal，数据变化重置key
- * @param {{
- *  modalConf?: Omit<ModalProps, 'onOk'>
- * } & Parameters<useForm>[0]} props 
- */
-const KModalKey = (props) => {
-  const { key } = useFormState(props.originData)
-  return <KModalWithForm {...props} key={key} />
-}
-
-export {
-  KModal as KModalPure,
-  KModalKey
-}
-
-export default KModalWithForm
