9c2d7a3598953e7a6c9866d02eed30412a6e8a02
odanzhou <ihosalt@gmail.com>
2022-09-26 15:31:10 +0800

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,10 @@
-export { default as useNewRef } from './useNewRef'
+export { default as useNewRef } from './useNewRef';
+export { default as useLoading } from './useLoading';
 export { default as useRequest } from './useRequest'
 export { default as useIsUpdate } from './useIsUpdate'
+export { default as useArrayLoading } from './useArrayLoading'
 export { default as useParams } from './useParams'
+export { default as useCurrenciesList } from './useCurrenciesList'
+export { default as useCallbackFn } from './useCallbackFn'
+export { default as useSearchConf } from './useSearchConf'
+export { default as useRefWrapper, useRefWrapperAuto } from './useRefWrapper'
