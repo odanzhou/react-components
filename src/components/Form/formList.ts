89e00337be7fbe4e1101e5b21a02ca729fe42595
odanzhou <ihosalt@gmail.com>
2023-01-25 10:45:43 +0800

    Update formList.ts

--- src/components/Form/formList.ts
+++ src/components/Form/formList.ts
@@ -1,6 +1,7 @@
 import React, { memo, useMemo } from 'react'
 import { Form, Row, Spin  } from 'antd'
 import cls from 'classnames';
+import { each } from 'lodash'
 import styles from './styles/index.less'
 import { DefLabelColSpan, DefLoading, DefUseRow } from './constants'
 
@@ -12,6 +13,7 @@ import { DefLabelColSpan, DefLoading, DefUseRow } from './constants'
  *  loading?: boolean | SpinProps,
  *  footerContent?: React.ReactNode,
  *  formItems?: React.ReactNode,
+ *  rowIndexList?: number[],
  *  formProps?: FormProps,
  *  labelColSpan?: number,
  *  wrapperColSpan?: number,
@@ -20,19 +22,39 @@ import { DefLabelColSpan, DefLoading, DefUseRow } from './constants'
  */
 const FormList = (props) => {
   const { useRow = DefUseRow, rowProps, loading = DefLoading, footerContent,
-    formItems, formProps, labelColSpan = DefLabelColSpan, wrapperColSpan, compact,
+    formItems, rowIndexList, formProps, labelColSpan = DefLabelColSpan, wrapperColSpan, compact,
   } = props
-
   const spinObj = useMemo(() => {
     return typeof loading === 'boolean' ? { spinning: loading } : (loading || {})
   }, [loading])
+  const rowList = useMemo(() => {
+    let count = 0
+    const arr =[[]]
+    if(!rowIndexList?.length) {
+      arr[0] = formItems
+    } else {
+      each(formItems, (value, index) => {
+        if(index !== 0 && rowIndexList.includes(index)) {
+          count += 1
+        }
+        if(!Array.isArray(arr[count])) {
+          arr[count] = [value]
+        } else {
+          arr[count].push(value)
+        }
+      })
+    }
+    return arr
+  }, [useRow, rowIndexList, formItems])
 
   return (
     <Spin {...spinObj}>
       <div className={cls({[styles.compact]: compact})}>
         <Form labelCol={{ span: labelColSpan }} wrapperCol={{ span: wrapperColSpan || 24 - labelColSpan }} {...formProps}>
           {
-            useRow ? <Row gutter={24} {...rowProps}>{formItems}</Row> : formItems
+            useRow ? <>
+              { rowList.map((items, index) => <Row gutter={24} {...rowProps} key={String(index)}>{items}</Row>)}
+            </> : formItems
           }
         </Form>
         { footerContent }
