97a6789bd387b7b7564a1d7edac7e7c103e78339
odanzhou <ihosalt@gmail.com>
2023-01-25 10:49:34 +0800

    Update itemConfirm.ts

--- src/components/Modal/itemConfirm.ts
+++ src/components/Modal/itemConfirm.ts
@@ -4,7 +4,7 @@ import { _t } from 'utils/i18n';
 import { useRequest } from 'src/hooks'
 import ConfirmButton from './confirmButton'
 
-const getParamsId = data => ({ id: data?.id })
+const getParamsId = (data, keyField) => ({ [keyField]: data?.[keyField] })
 
 /**
  * 删除操作
@@ -12,14 +12,15 @@ const getParamsId = data => ({ id: data?.id })
  *  onOk(isDel: boolean): void,
  *  fetchApi: (params) => Promise<any>,
  *  data?: Record<string, any>,
- *  getParams?: (data: Record<string, any>) => Record<string, any>, // 默认获取 id {id: string}
+ *  getParams?: (data: Record<string, any>) => Record<string, any>, // 默认获取 id {id: string},
+ *  keyField?: string,
  * } & Omit<Parameters<ConfirmButton>[0], 'loading' | 'onOk'>} props
  */
 const ItemConfirm = (props) => {
-  const { data, onOk, fetchApi, getParams = getParamsId, params: paramsProps, ...others } = props
+  const { data, onOk, fetchApi, getParams = getParamsId, params: paramsProps, keyField = 'id', ...others } = props
   const params = useMemo(() => {
-    return typeof getParams === 'function' ? getParams(data) : data
-  }, [data, getParams])
+    return typeof getParams === 'function' ? getParams(data, keyField) : data
+  }, [data, getParams, keyField])
 
   const { onFetch, loading } = useRequest(fetchApi, { manual: true, isSilence: false, fetchHandleConf: {
     onOk() {
