929298b213d521afe346d6f0610d510dbeb4766f
odanzhou <ihosalt@gmail.com>
2023-03-23 14:16:54 +0800

    Update numberSuffix.tsx

--- src/components/Input/numberSuffix.tsx
+++ src/components/Input/numberSuffix.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useMemo } from 'react'
 import { InputNumber } from 'antd'
 import style from './style.less'
 
@@ -6,9 +6,19 @@ import style from './style.less'
  * 具有后缀的数字输入框
  */
 const NumberSuffix = forwardRef((props, ref) => {
-  const { suffix, ...others } = props
+  const { suffix, autoPrecision, ...others } = props
+  const { value } = others
+  // 低优先级的 precision
+  const precision = useMemo(() => {
+    if(!autoPrecision) return undefined
+    const pointVal = String(value).split('.')[1]
+    if(!pointVal) return
+    // 处理指数问题
+    const [pointLen, eLenght] = pointVal.split('e-')
+    return pointLen.length + (Number(eLenght || 0) || 0)
+  }, [value, autoPrecision])
   return <div className={style.number_suffix}>
-    <InputNumber {...others} ref={ref} />
+    <InputNumber precision={precision} {...others} ref={ref} />
     <div className='number_suffix'>{suffix}</div>
   </div>
 })
