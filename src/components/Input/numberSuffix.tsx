a660fee64fa79d25e39e198b856ca97a6666b017
odanzhou <ihosalt@gmail.com>
2023-04-06 10:10:27 +0800

    Update numberSuffix.tsx

--- src/components/Input/numberSuffix.tsx
+++ src/components/Input/numberSuffix.tsx
@@ -7,16 +7,7 @@ import style from './style.less'
  */
 const NumberSuffix = forwardRef((props, ref) => {
   const { suffix, autoPrecision, ...others } = props
-  const { value } = others
-  // 低优先级的 precision
-  const precision = useMemo(() => {
-    if(!autoPrecision) return undefined
-    const pointVal = String(value).split('.')[1]
-    if(!pointVal) return
-    // 处理指数问题
-    const [pointLen, eLenght] = pointVal.split('e-')
-    return pointLen.length + (Number(eLenght || 0) || 0)
-  }, [value, autoPrecision])
+  const precision = autoPrecision > 8 ? autoPrecision : undefined
   return <div className={style.number_suffix}>
     <InputNumber precision={precision} {...others} ref={ref} />
     <div className='number_suffix'>{suffix}</div>
