edd78781ae560795fa4a2a7215f2e59b99782035
odanzhou <ihosalt@gmail.com>
2023-07-19 10:50:07 +0800

    Update helper.ts

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,3 +1,5 @@
+import Decimal from 'decimal.js/decimal';
+
 /**
  * 精度转小数格式，eg. 8 -> 0.00000001
  */
@@ -7,4 +9,19 @@ export const precision2float = (precision) => {
     return 0;
   }
   return new Decimal(10).pow(-1 * precision).toFixed();
+};
+
+/**
+ * 判断值是否为空值
+ * @param {*} val
+ */
+export const valIsEmpty = val => val == null || String(val).trim() === '';
+
+/**
+ * 判断是否满足最小精度
+ * @param {any} val
+ * @param {number} step
+ */
+export const isMinStep = (val, step) => {
+  return +Decimal.mod(val, step || 1).toFixed() === 0;
 };
